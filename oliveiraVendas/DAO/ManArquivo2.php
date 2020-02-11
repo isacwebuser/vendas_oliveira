5587de6bb4460bf4a479e4321d6d3381c263ba3f
isacwebuser <isacrgs@gmail.com>
2020-02-11 07:06:49 -0300

    incluindo função csv

--- oliveiraVendas/DAO/ManArquivo2.php
+++ oliveiraVendas/DAO/ManArquivo2.php
@@ -2,25 +2,43 @@
 
 require_once("config.php");
 
-$sql = new Banco();
-$users = $sql->select("SELECT * FROM tab_usuario ORDER BY desLogin");
+class Exportar{
 
-$headers = array();
 
-//Foreach de cabeçalho
-foreach ($users[0] as $key => $value) {
-    array_push($headers, ucfirst($key));
-}
-$file = fopen("usuarios.csv", "w+");
-fwrite($file, implode(",", $headers) . "\r\n");
-//Foreach de linha
-foreach ($users as $rows) {
-    $data = array();
-    //Foreach de coluna
-    foreach ($rows as $columns => $value) {
-        array_push($data, $value);
+private $headers = array();
+
+    public function getHeaders(){
+        return $this->headers;
     }
-    fwrite($file, implode(",", $data) . "\r\n");
-}
+    public function setHeaders($headers){
+        $this->headers = $headers;
+    }
 
-fclose($file);
+    public function montarCSV(){
+
+        $users = new Usuario();
+        $saida = $users->loadById(10);
+//Foreach de cabeçalho
+        if(count($saida) > 0){
+
+        foreach ($saida[0] as $key => $value) {
+            array_push($headers, ucfirst($key));
+        }
+        $file = fopen("usuarios.csv", "w+");
+        fwrite($file, implode(",", $headers) . "\r\n");
+//Foreach de linha
+        foreach ($saida as $rows) {
+            $data = array();
+            //Foreach de coluna
+            foreach ($rows as $columns => $value) {
+                array_push($data, $value);
+            }
+            fwrite($file, implode(",", $data) . "\r\n");
+        }
+
+        fclose($file);
+    } else {
+            echo "Nenhum registro encontrado.";
+        }
+    }
+}
